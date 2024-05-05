bb5e9f500dac94e634f3274899313da5039a0ac9
Bogdan3-web <156899933+Bogdan3-web@users.noreply.github.com>
2024-05-06 00:58:50 +0200

    Update LKMJNHBV.js

--- LKMJNHBV.js
+++ LKMJNHBV.js
@@ -1,9 +1,15 @@
 const hintButton = document.querySelector('.hint');
 const img = document.getElementById('img');
+const lastNextBtn = document.getElementById('lastNextBtn');
 
 const displayNone = () => {
     hintButton.style.display = 'none';
     img.classList.remove('display-none');
+    setTimeout(() => {
+        img.classList.add('display-none');
+        lastNextBtn.classList.remove('display-none');
+    }, 5000);
 };
 
 hintButton.addEventListener('click', displayNone);
+
